00b511a4c0fb5b59e1884e0756cb3cb9a5b7fae7
nogandalfgrey <no.gandalf.grey@gmail.com>
2019-07-21 15:18:43 +0300

    Updates

---
+++ precache-manifest.62e0c4c6cb8802a30f938aa9eca5ad8d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f9b097049ba7ffbd77bcd906eadc409",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "3e60d07258228c2aaf25",
+    "url": "/todo-list/static/css/main.ea83e963.chunk.css"
+  },
+  {
+    "revision": "06f44d7c6eeee966ea35",
+    "url": "/todo-list/static/js/2.34ad89f7.chunk.js"
+  },
+  {
+    "revision": "3e60d07258228c2aaf25",
+    "url": "/todo-list/static/js/main.ecc1a6f4.chunk.js"
+  },
+  {
+    "revision": "ca797cc406ded84cf733",
+    "url": "/todo-list/static/js/runtime~main.41b69a58.js"
+  }
+]);
